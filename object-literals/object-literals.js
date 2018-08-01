e382552779f42486343e27286d9752acd601cc43
yousef19-meet <yousef19@meet.mit.edu>
2018-08-01 14:09:07 +0300

    ok

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me ={
+	firstName:"Yousef___",
+	lastName:"   Michael___",
+	favoriteFoods:["   mqlobe", "pizza___"],
+	age:16,
+	picture:"   google.com___"
+}
+var myjason = JSON.stringify(me);
+document.write(myjason)
--- separation/code.js
+++ separation/code.js
@@ -1,1 +1,1 @@
-// Add your alert() here!
+alert("YOOO WADAAPPPP");
